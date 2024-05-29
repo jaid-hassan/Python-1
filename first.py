e78598ee7385be3ea47c226d8e96ed35474e7b70
Jaid Hassan <jaidhassan7200@gmail.com>
2024-05-29 13:47:29 +0600

    new

---
+++ first.py
@@ -0,0 +1,6 @@
+name=input("Enter name:")
+age=input("Enter age:")
+address=input("Enter address:")
+print ("your name:",name)
+print ("your age:",age)
+print ("your address:",address)
---
+++ second.py
@@ -0,0 +1,7 @@
+name = input("enter any name : ")
+age = input("enter your age :")
+address = input ("enter addresss:")
+
+print("my name :", name)
+print("my age :", age)
+print("my address :", address)
