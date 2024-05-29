aecddb8cbc38ac94159547ab992f02d3b3cc6bcf
Jaid Hassan <jaidhassan7200@gmail.com>
2024-05-29 23:18:23 +0600

    new

---
+++ second hw.py
@@ -0,0 +1,3 @@
+a = 10
+b = 5
+print =("The result of a+b =",a+b)
--- second.py
+++
@@ -1,7 +0,0 @@
-name = input("enter any name : ")
-age = input("enter your age :")
-address = input ("enter addresss:")
-
-print("my name :", name)
-print("my age :", age)
-print("my address :", address)
