86c5cd04a100e89531fd1f8cda1fdf343c76c46e
Jaid Hassan <jaidhassan7200@gmail.com>
2024-05-30 00:19:38 +0600

    new

---
+++ secondhw.py
@@ -0,0 +1,35 @@
+# 1. Perform calculations and print results
+a = 10
+b = 5
+print ("The result of a+b =", a+b)
+print ("The result of a-b =", a-b)
+print ("The result of a*b =", a*b)
+print ("The result of a**b =", a**b)
+
+# 2. Variable conversion and string manipulation
+x = 25
+y = 15.5
+
+x_str = str(x)
+y_int = int(y)
+
+concatenated_string = x_str + " apples"
+
+print(concatenated_string)
+print(y_int)
+
+# 3. Logical operators
+is_sunny = True
+is_rainy = False
+
+print("is_sunny and is_rainy:",is_sunny and is_rainy)
+print("is_sunny or is_rainy:",is_sunny or is_rainy)
+print("not is_sunny:",not is_sunny)
+
+# 4. Calculate and print rectangle area (rounded down to integer)
+width = 4.5
+height = 7.8
+
+area = int(width * height)
+
+print("Area of rectangle:",area)
