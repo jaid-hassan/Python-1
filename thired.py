b31a1e8c5af99d9e0e3c2e5ceabbcf8e459a5f1c
jaid hassan <jaidhassan7200@gmail.com>
2024-06-01 00:07:38 +0600

    new

--- thired.py
+++ thired.py
@@ -5,3 +5,11 @@ print ("The result of a+b =", a+b)
 print ("The result of a-b =", a-b)
 print ("The result of a*b =", a*b)
 print ("The result of a**b =", a**b)
+# 2.........
+x = 25
+y = 15.5
+x_str = str(x)
+y_int = int(y)
+concatenated_string = x_str + " apples"
+print(concatenated_string)
+print(y_int)
