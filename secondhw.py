6434337cffde86ebd6ee2a36e1c9c499cc8a1c2d
jaid hassan <jaidhassan7200@gmail.com>
2024-06-02 20:48:39 +0600

    ,,

--- secondhw.py
+++ secondhw.py
@@ -1,30 +1,26 @@
-# 1............
 a = 10
 b = 5
-print ("The result of a+b =", a+b)
-print ("The result of a-b =", a-b)
-print ("The result of a*b =", a*b)
-print ("The result of a**b =", a**b)
+print (" a+b =", a+b)
+print (" a-b =", a-b)
+print (" a*b =", a*b)
+print (" a**b =", a**b)
 
-# 2...........
 x = 25
 y = 15.5
-x_str = str(x)
-y_int = int(y)
+xstring = str(x)
+yinteger = int(y)
 
-concatenated_string = x_str + " apples"
-print(concatenated_string)
-print(y_int)
+concatenatedstring = xstring + " apples"
+print(concatenatedstring)
+print(yinteger)
 
-# 3............
 is_sunny = True
 is_rainy = False
 print("is_sunny and is_rainy:",is_sunny and is_rainy)
 print("is_sunny or is_rainy:",is_sunny or is_rainy)
 print("not is_sunny:",not is_sunny)
 
-# 4.............
 width = 4.5
 height = 7.8
 area = int(width * height)
-print("Area of rectangle:",area)
+print("Area of the rectangle:",area)
