0bf7c53b9a28c3b21d8a9e9c1d9e7302d39e018e
jaid hassan <jaidhassan7200@gmail.com>
2024-06-02 21:02:57 +0600

    ;

--- secondhw.py
+++ secondhw.py
@@ -1,24 +1,28 @@
+# 1...........
 a = 10
 b = 5
-print (" a+b =", a+b)
-print (" a-b =", a-b)
-print (" a*b =", a*b)
-print (" a**b =", a**b)
+print ("The result of a+b =", a+b)
+print ("The result of a-b =", a-b)
+print ("The result of a*b =", a*b)
+print ("The result of a**b =", a**b)
+# 2...........
 
 x = 25
 y = 15.5
-xstring = str(x)
-yinteger = int(y)
+x_str = str(x)
+y_int = int(y)
 
-concatenatedstring = xstring + " apples"
-print(concatenatedstring)
-print(yinteger)
+concatenated_str= x_str + " apples"
+print(concatenated_str)
+print(y_int)
+# 3..........
 
 is_sunny = True
 is_rainy = False
 print("is_sunny and is_rainy:",is_sunny and is_rainy)
 print("is_sunny or is_rainy:",is_sunny or is_rainy)
 print("not is_sunny:",not is_sunny)
+# 4...........
 
 width = 4.5
 height = 7.8
