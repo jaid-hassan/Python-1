109dd1d198ca5442ff3368528bbca3771891ef19
jaid hassan <jaidhassan7200@gmail.com>
2024-06-05 00:04:55 +0600

    op

--- hw4.py
+++ hw4.py
@@ -3,10 +3,10 @@ num1 = int (input("Enter the first number: "))
 num2 = int (input("Enter the second number: "))
 
 if operator == "+" :
-    print("The result is: ", int(num1+num2))
+    print("The result is: ",int(num1+num2))
 if operator == "-" :
-    print("The result is: ", int(num1-num2))
+    print("The result is: ",int(num1-num2))
 if operator == "*" :
-    print("The result is: ", int(num1*num2))
+    print("The result is: ",int(num1*num2))
 if operator == "/" :
-    print("The result is: ", int(num1/num2))
+    print("The result is: ",int(num1/num2))
