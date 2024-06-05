32e6689f061b7f75c23915d1bec71aa6e886a567
jaid hassan <jaidhassan7200@gmail.com>
2024-06-05 22:22:00 +0600

    jk

--- hw4.py
+++ hw4.py
@@ -1,12 +1,3 @@
-operator = input("Enter a operator(  +, _ , * , / ) : ")
-num1 = int (input("Enter the first number: "))
-num2 = int (input("Enter the second number: "))
-
-if operator == "+" :
-    print("The result is: ",int(num1+num2))
-if operator == "-" :
-    print("The result is: ",int(num1-num2))
-if operator == "*" :
-    print("The result is: ",int(num1*num2))
-if operator == "/" :
-    print("The result is: ",int(num1/num2))
+for i in range=(3):
+    for j in range=(2):
+        print()
