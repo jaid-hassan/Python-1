35b1ae8c25fc41a8b53a78591eaf4fca0fc25560
jaid hassan <jaidhassan7200@gmail.com>
2024-06-07 19:01:41 +0600

    shahed

--- hw5.py
+++ hw5.py
@@ -1,4 +1,1 @@
-num = int(input("Enter a number : "))
-for i in num :
-    for j in range(1,11) :
-        print (f"{num}*{j}= {num*j}")
+ # Jaid hassan is a very good person. Rashid is a bedi pagol. 
